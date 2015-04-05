f7c5a794a19ab4fb43c9f9a99ae625346c03449b
Andrew Hartwig <andrew.hartwig1@gmail.com>
2015-04-04 23:16:58 -0400

    Added toggle functionality to buttons.

--- res/js/main.js
+++ res/js/main.js
@@ -1,5 +1,27 @@
+$('#menu button').on('click', function(event) {
+    if (!$(this).hasClass('active')) {
+        $('#menu button').removeClass('active');
+        $(this).addClass('active');
+
+        switch($(this).attr('id')) {
+            case "parts":
+                getPart();
+                break;
+            case "vendors":
+                getVendor();
+                break;
+            case "edit":
+                break;
+            case "reset":
+                break;
+        }
+    } else {
+        $('#menu button').removeClass('active');
+        $('#dynamic')[0].innerHTML = "";
+    }
+});
+
 function getPart() {
-    $(this).addClass("active");
     var rhttp = new XMLHttpRequest();
     rhttp.onreadystatechange = function() {
         if (rhttp.readyState == 4 && rhttp.status == 200) {
@@ -8,6 +30,17 @@ function getPart() {
         }
     }
     rhttp.open("GET", "res/php/part.php", true);
-    console.log("hi mom!");
+    rhttp.send();
+}
+
+function getVendor() {
+    var rhttp = new XMLHttpRequest();
+    rhttp.onreadystatechange = function() {
+        if (rhttp.readyState == 4 && rhttp.status == 200) {
+            document.getElementById("dynamic").innerHTML = rhttp.responseText;
+
+        }
+    }
+    rhttp.open("GET", "res/php/vendor.php", true);
     rhttp.send();
 }
